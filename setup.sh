7d5c475dd896f7a160295e4d5a475659edc3ba11
Darianopolis <darianopolis@gmail.com>
2024-06-25 11:56:06 +0100

    Using VS Code instead of Code OSS; Switched to minimal KDE Plasma package

--- setup.sh
+++ setup.sh
@@ -8,4 +8,4 @@ ln -fs $(pwd)/term/.bashrc ~/.bashrc
 ln -fs $(pwd)/term/.blerc ~/.blerc
 ln -f  $(pwd)/term/kitty.conf ~/.config/kitty/kitty.conf
 
-ln -fs $(pwd)/ide/vscode.json $HOME/.config/Code\ -\ OSS/User/settings.json
+ln -fs $(pwd)/ide/vscode.json $HOME/.config/Code/User/settings.json
