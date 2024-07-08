c08814e3cb9fd702a622d4d28806949f934bb839
Darianopolis <darianopolis@gmail.com>
2024-07-08 02:33:44 +0100

    Huge restructure on new intsall

--- arch/generate-links.sh
+++ arch/generate-links.sh
@@ -4,8 +4,8 @@ cd "$(dirname "$0")"
 
 echo "Setting up dotfiles from: $(pwd)"
 
-ln -fs $(pwd)/term/.bashrc ~/.bashrc
-ln -fs $(pwd)/term/.blerc ~/.blerc
-ln -f  $(pwd)/term/kitty.conf ~/.config/kitty/kitty.conf
+ln -fs $(pwd)/terminal/.bashrc ~/.bashrc
+ln -fs $(pwd)/terminal/.blerc ~/.blerc
+ln -f  $(pwd)/terminal/kitty.conf ~/.config/kitty/kitty.conf
 
 ln -fs $(pwd)/../vscode/settings.json $HOME/.config/Code/User/settings.json
