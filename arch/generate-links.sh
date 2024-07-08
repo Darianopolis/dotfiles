c2ee85f4332655c7e006ee639855f4dbe370bfc8
Darianopolis <darianopolis@gmail.com>
2024-07-08 04:38:32 +0100

    Added Ark; Moved GCM to development

--- arch/generate-links.sh
+++ arch/generate-links.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 echo "Setting up dotfiles from: $(pwd)"
 
 ln -fs $(pwd)/terminal/.bashrc ~/.bashrc
-ln -fs $(pwd)/terminal/.blerc ~/.blerc
-ln -f  $(pwd)/terminal/kitty.conf ~/.config/kitty/kitty.conf
+# ln -fs $(pwd)/terminal/.blerc ~/.blerc
+# ln -f  $(pwd)/terminal/kitty.conf ~/.config/kitty/kitty.conf
 
 ln -fs $(pwd)/../vscode/settings.json $HOME/.config/Code/User/settings.json
