d80b47043ea8e796dd94e9349c116d560136136a
Darianopolis <darianopolis@gmail.com>
2025-08-30 22:48:37 +0100

    moved some applications from desktop to extra

--- arch/configure.sh
+++ arch/configure.sh
@@ -9,7 +9,7 @@ echo "Setting up dotfiles from: $(pwd)"
 cp $(pwd)/terminal/.bashrc ~/.bashrc
 # cp $(pwd)/terminal/kitty.conf ~/.config/kitty/kitty.conf
 
-# cp $(pwd)/../vscode/settings.json $HOME/.config/Code/User/settings.json
+cp $(pwd)/../vscode/settings.json $HOME/.config/Code/User/settings.json
 
 sudo cp $(pwd)/update.sh /usr/local/bin/update
 sudo chmod +x /usr/local/bin/update
